d6c3ec1deda61940dc6f20800ef386498c92fae2
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-11-15 21:47:06 +0700

    signup functionality;

---
+++ app/(authentication)/signup/page.tsx
@@ -0,0 +1,11 @@
+import SignupForm from "@/app/components/Form/SignupForm";
+
+export default function Signup() {
+  return (
+    <>
+      <title>Signup</title>
+
+      <SignupForm />
+    </>
+  );
+}
