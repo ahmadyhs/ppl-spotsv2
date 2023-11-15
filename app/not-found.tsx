a9200b399a1b61955e02916f09bff52ec192c9a2
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-11-15 22:01:54 +0700

    not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="bg-darkblue flex h-screen items-center justify-center">
+      <p className="text-center text-lg text-white">
+        <span className="font-semibold">Laman Tidak Ditemukan!</span> <br />
+        Konten yang anda cari tidak sesuai atau salah
+      </p>
+    </div>
+  );
+}
