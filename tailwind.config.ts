734d1329751318785edc0223b8e389c1c165ff7b
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-11-17 11:11:54 +0700

    update config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,26 @@ const config: Config = {
       colors: {
         darkblue: "#0f172a",
       },
+      animation: {
+        slideup: "slideup 0.75s ease-in-out",
+        slideleft: "slideleft 1.25s ease-in-out",
+        slideleftonly: "slideleftonly 5s ease-in",
+      },
+      keyframes: {
+        slideup: {
+          "0%, 75%": { opacity: "0", transform: "translateY(0.5rem)" },
+
+          "100%": { opacity: "100", transform: "translateY(0rem)" },
+        },
+        slideleft: {
+          "0%, 75%": { opacity: "0", transform: "translateX(-2rem)" },
+          "100%": { opacity: "100", transform: "translateX(0rem)" },
+        },
+        slideleftonly: {
+          "0%": { width: "20px" },
+          "100%": { width: "80%" },
+        },
+      },
     },
   },
   plugins: [],
