3a5f13c88d704b5516fa640016f301938c267ba8
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-12-13 20:38:31 +0700

    adjust table view for admin

--- app/admin/penyedia/page.tsx
+++ app/admin/penyedia/page.tsx
@@ -53,14 +53,14 @@ export default function AdminPenyedia() {
       <table className="table-container h-5/6 ">
         <thead className="table-head">
           <tr className="flex w-full items-center rounded-tl-xl rounded-tr-xl bg-darkgray text-center">
-            <th className="w-10 p-2 font-medium text-white">Owner ID</th>
-            <th className="w-20 p-2 font-medium text-white">Nama Depan</th>
-            <th className="w-20 p-2 font-medium text-white">Nama Belakang</th>
-            <th className="w-40 p-2 font-medium text-white">Email</th>
-            <th className="w-32 p-2 font-medium text-white">Nomor Telepon</th>
-            <th className="w-20 p-2 font-medium text-white">Bank</th>
-            <th className="w-32 p-2 font-medium text-white">Nomor Kartu</th>
-            <th className="w-40 p-2 font-medium text-white">NIK</th>
+            <th className="w-24 p-2 font-medium text-white">Owner ID</th>
+            <th className="w-36 p-2 font-medium text-white">Nama Depan</th>
+            <th className="w-36 p-2 font-medium text-white">Nama Belakang</th>
+            <th className="w-52 p-2 font-medium text-white">Email</th>
+            <th className="w-36 p-2 font-medium text-white">Nomor Telepon</th>
+            <th className="w-32 p-2 font-medium text-white">Bank</th>
+            <th className="w-64 p-2 font-medium text-white">Nomor Kartu</th>
+            <th className="w-52 p-2 font-medium text-white">NIK</th>
             <th className="w-56 p-2 font-medium text-white">KTP </th>
             <th className="w-40 p-2 font-medium text-white">Status</th>
             <th className="w-40 p-2 font-medium text-white">Edit Status</th>
@@ -74,18 +74,18 @@ export default function AdminPenyedia() {
               owners.map((o) => {
                 return (
                   <tr className="flex w-full items-center" key={o.owner_id}>
-                    <td className="w-10 p-2">{o.owner_id}</td>
-                    <td className="w-20 break-words p-2">
+                    <td className="w-24 p-2">{o.owner_id}</td>
+                    <td className="w-36 break-words p-2">
                       {o.user.first_name}
                     </td>
-                    <td className="w-20 break-words p-2">{o.user.last_name}</td>
-                    <td className="w-40 break-words p-2">{o.user.email}</td>
-                    <td className="w-32 break-words p-2">
+                    <td className="w-36 break-words p-2">{o.user.last_name}</td>
+                    <td className="w-52 break-words p-2">{o.user.email}</td>
+                    <td className="w-36 break-words p-2">
                       {o.user.phone_number}
                     </td>
-                    <td className="w-20 break-words p-2">{o.bank_name}</td>
-                    <td className="w-32 break-words p-2">{o.card_number}</td>
-                    <td className="w-40 break-words p-2">{o.nik}</td>
+                    <td className="w-32 break-words p-2">{o.bank_name}</td>
+                    <td className="w-64 break-words p-2">{o.card_number}</td>
+                    <td className="w-52 break-words p-2">{o.nik}</td>
                     <td className="w-56 p-2">
                       {o.ktp_picture && (
                         <Image
@@ -98,7 +98,7 @@ export default function AdminPenyedia() {
                             height: "100%",
                           }}
                           priority
-                          className="rounded-sm"
+                          className="mx-auto aspect-video rounded-sm object-cover"
                         />
                       )}
                     </td>
