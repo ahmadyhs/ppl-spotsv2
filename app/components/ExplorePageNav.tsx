4b68610d19a18d84375419b1b7e90d5a497709d3
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2023-12-14 11:22:08 +0700

    readjust explore component and refactor

--- app/components/ExplorePageNav.tsx
+++ app/components/ExplorePageNav.tsx
@@ -1,39 +1,71 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 
-export default function PageNav({
-  pageIndex,
-  direction,
+export default function ExplorePageNav({
+  prevIndex,
+  currentIndex,
+  nextIndex,
 }: {
-  pageIndex: number | null;
-  direction: "PREV" | "NEXT";
+  prevIndex: number | null;
+  currentIndex: number;
+  nextIndex: number | null;
 }) {
-  const { push } = useRouter();
-  const navArrow =
-    direction === "PREV" ? (
-      <GoChevronLeft size="1.5em" color="white" />
-    ) : (
-      <GoChevronRight size="1.5em" color="white" />
+  function pageNav(pageIndex: number | null, direction: "PREV" | "NEXT") {
+    const navArrow =
+      direction === "PREV" ? (
+        <GoChevronLeft size="1.5em" color="white" />
+      ) : (
+        <GoChevronRight size="1.5em" color="white" />
+      );
+    return (
+      <>
+        {pageIndex ? (
+          <Link
+            href={`/eksplorasi?page=${pageIndex}`}
+            className="flex items-center rounded-md bg-darkblue p-2"
+          >
+            {navArrow}
+          </Link>
+        ) : (
+          <div className="flex items-center rounded-md bg-darkblue bg-opacity-50 p-2">
+            {navArrow}
+          </div>
+        )}
+      </>
     );
+  }
   return (
     <>
-      {pageIndex ? (
-        <button
-          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-            e.preventDefault();
-            push(`/eksplorasi?page=${pageIndex}`);
-          }}
-          className="flex items-center rounded-md bg-darkblue p-2"
-        >
-          {navArrow}
-        </button>
-      ) : (
-        <div className="flex items-center rounded-md bg-darkblue bg-opacity-50 p-2">
-          {navArrow}
+      <div className="sticky top-52 z-10 flex h-12 w-full justify-center gap-x-12 bg-white/95 pb-2 stm:top-36 stm:gap-x-24">
+        {pageNav(prevIndex, "PREV")}
+
+        <div className="flex flex-col justify-center text-center">
+          <p>Halaman</p>
+          <div className="grid w-max cursor-default grid-flow-col gap-x-2">
+            <p
+              className={
+                (!prevIndex ? "opacity-0 " : "bg-slate-100 ") + "w-6 rounded-md"
+              }
+            >
+              {prevIndex}
+            </p>
+
+            <p className={"w-6 rounded-md bg-blue-300"}>{currentIndex}</p>
+
+            <p
+              className={
+                (!nextIndex ? "opacity-0 " : "bg-slate-100 ") + "w-6 rounded-md"
+              }
+            >
+              {nextIndex}
+            </p>
+          </div>
         </div>
-      )}
+
+        {pageNav(nextIndex, "NEXT")}
+      </div>
     </>
   );
 }
